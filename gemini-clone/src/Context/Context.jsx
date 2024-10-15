3832f904df82111e5079735d438a24d93632561c
atlaryea <manyeadorkor5@gmail.com>
2024-10-15 16:19:39 +0000

    lil update in main/main

--- gemini-clone/src/Context/Context.jsx
+++ gemini-clone/src/Context/Context.jsx
@@ -1,18 +1,33 @@
-import { createContext } from "react";
+import { createContext, useState } from "react";
 import runChat from '../config/gemini';
 
 export const Context = createContext();
 
 const Contextprovider = (props) => {
 
+    const [input, setInput] = useState("");
+    const [recentPrompt, setRecentPrompt] = useState("");
+    const [prevPrompts, setPrevPrompts] = useState("");
+    const [showResult, setShowResult] = useState("");
+    const [loading, setLoading] = useState("");
+    const [resultData, setResultData] = useState("");
+
     const onSent = async (prompt) => {
         await runChat(prompt)
     }
 
-    onSent("What is react js")
-
     const contextValue = {
-
+        prevPrompts,
+        setPrevPrompts,
+        onSent,
+        recentPrompt,
+        setRecentPrompt,
+        showResult,
+        setShowResult,
+        loading,
+        resultData,
+        input,
+        setInput,
     }
 
     return(
--- gemini-clone/src/components/Main/Main.jsx
+++ gemini-clone/src/components/Main/Main.jsx
@@ -1,8 +1,12 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import './Main.css'
 import { assets } from '../../assets/assets'
+import { Context } from '../../Context/Context'
 
 const Main = () => {
+
+  const {onSent, recentPrompt, showResult, loading, resultData, input, setInput} = useContext(Context)
+
   return (
     <div className='main'>
       <div className='nav'>
