989a0ed69888cc077f7fd338badbd13e34019ebc
atlaryea <manyeadorkor5@gmail.com>
2024-10-17 09:47:04 +0000

    added the code to format the response by removing the asterisk

--- gemini-clone/src/Context/Context.jsx
+++ gemini-clone/src/Context/Context.jsx
@@ -12,6 +12,10 @@ const Contextprovider = (props) => {
     const [loading, setLoading] = useState("");
     const [resultData, setResultData] = useState("");
 
+    const delapPara = (index, nextWord) =>{
+
+    }
+
     const onSent = async (prompt) => {
 
         setResultData("")
@@ -19,7 +23,19 @@ const Contextprovider = (props) => {
         setShowResult(true)
         setRecentPrompt(input)
         const response = await runChat(input)
-        setResultData(response)
+        let responseArray = response.split("**")
+        let newResponse;
+        for(let i=0; i < responseArray.length; i++)
+        {
+            if(i === 0 || i%2 !== 1){
+                newResponse += responseArray[i];
+            }
+            else{
+                newResponse += "<b>"+responseArray[i]+"</b>"
+            }
+        }//this code removes the asterisk from the response
+        let newResponse2 = newResponse.split("*").join("</br>")
+        setResultData(newResponse2)
         setLoading(false)
         setInput("")
     }
